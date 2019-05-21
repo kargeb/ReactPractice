6a15dd393a69f2a0181ccac9c5115daafd680cb3
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-05-21 21:02:09 +0200

    after project structure reirganization

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>Hello Pies</h1>
+    <Button>Przycisk Pies</Button>
+  </div>
+);
+
+export default Root;
