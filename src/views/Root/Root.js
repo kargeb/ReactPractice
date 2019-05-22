ca945b295838d07073cd955164e2a04d8384d710
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-05-22 10:55:21 +0200

    after styled components

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>Hello Pies</h1>
-    <Button>Przycisk Pies</Button>
+    <Button width={100}>Open / Close</Button>
+    <Button secondary>Remove</Button>
   </div>
 );
 
