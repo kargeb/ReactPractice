40dc3483aa6d94def2557432f84bd9d0f9a77fef
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-05-23 09:37:28 +0200

    Before creating the interface

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
+  background-color: ${({ theme }) => theme.primary};
   width: 220px;
   height: 47px;
   border: none;
@@ -20,5 +20,7 @@ const Button = styled.button`
       font-size: 10px;
     `}
 `;
+
+// background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
 
 export default Button;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Button from 'components/Button/Button';
+import Button from 'components/atoms/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/mainTheme';
 
 const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>Hello Pies</h1>
-    <Button width={100}>Open / Close</Button>
-    <Button secondary>Remove</Button>
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Hello Pies</h1>
+        <Button width={100}>Open / Close</Button>
+        <Button secondary>Remove</Button>
+      </>
+    </ThemeProvider>
   </div>
 );
 
